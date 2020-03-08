c8a0d4a62e86453f3aeabc27dbf721627360fc65
René Weiser <rene.weiser@uni-weimar.de>
2020-03-08 22:45:41 +0100

    Seeding roles and abilities

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(AbilitySeeder::class);
+        $this->call(RoleSeeder::class);
         $this->call(UsersSeeder::class);
     }
 }
