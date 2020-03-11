aab2e1fa8b736d75a97bae8a46c51516f41bd7e9
Rene <rene.weiser@uni-weimar.de>
2020-03-11 14:11:18 +0100

    applying some formatting

--- app/Project.php
+++ app/Project.php
@@ -5,8 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Models\Permission;
 
-class Project extends Model
-{
+class Project extends Model {
     protected $fillable = [
         'name', 'description'
     ];
@@ -27,7 +26,7 @@ class Project extends Model
 
     public function attachMember(User $user)
     {
-        $permission = Permission::firstOrcreate(['name' => "edit.projects.{$this->id}"]);
+        $permission = Permission::firstOrCreate(['name' => "edit.projects.{$this->id}"]);
         $user->givePermissionTo($permission);
         $this->users()->attach($user);
     }
@@ -37,18 +36,23 @@ class Project extends Model
         $users = $this->users()->get();
         $ownerPermission = Permission::where('name', "*.projects.{$this->id}")->first();
         $memberPermission = Permission::where('name', "edit.projects.{$this->id}")->first();
-        foreach ($users as $user) {
-            if ($ownerPermission !== null) {
+        foreach ($users as $user)
+        {
+            if ($ownerPermission !== null)
+            {
                 $user->revokePermissionTo($ownerPermission);
             }
-            if ($memberPermission !== null) {
+            if ($memberPermission !== null)
+            {
                 $user->revokePermissionTo($memberPermission);
             }
         }
-        if ($ownerPermission !== null) {
+        if ($ownerPermission !== null)
+        {
             $ownerPermission->delete();
         }
-        if ($memberPermission !== null) {
+        if ($memberPermission !== null)
+        {
             $memberPermission->delete();
         }
     }
