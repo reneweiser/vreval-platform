b526fe38a246d4b7c825374dd9021e5571725d15
René Weiser <rene.weiser@uni-weimar.de>
2020-03-11 22:21:24 +0100

    Cleaning up controllers

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -10,35 +10,40 @@ class ProjectController extends Controller
     public function create()
     {
         $this->authorize('create.projects');
+
         return view('project.create');
     }
 
-    public function store()
+    public function store(Request $request)
     {
         $this->authorize('create.projects');
-        $project = Project::create($this->validateProject());
+        $project = Project::create($this->validateProject($request));
         $project->attachOwner(auth()->user());
+
         return redirect(route('project.edit', ['project' => $project]));
     }
 
     public function show(Project $project)
     {
         $this->authorize("edit.projects.{$project->id}");
+
         return view('project.show', ['project' => $project]);
     }
 
     public function edit(Project $project)
     {
         $this->authorize("edit.projects.{$project->id}");
+
         return view('project.edit', ['project' => $project]);
     }
 
     public function update(Request $request, Project $project)
     {
         $this->authorize("edit.projects.{$project->id}");
-        $project->update($this->validateProject());
+        $project->update($this->validateProject($request));
         $project->save();
-        return view('project.edit', ['project' => $project]);
+
+        return redirect(route('project.edit', ['project' => $project]));
     }
 
     public function destroy(Project $project)
@@ -46,12 +51,13 @@ class ProjectController extends Controller
         $this->authorize("delete.projects.{$project->id}");
         $project->deletePermissions();
         $project->delete();
-        return redirect(route('home'));
+
+        return redirect(route('dashboard'));
     }
 
-    private function validateProject()
+    private function validateProject(Request $request)
     {
-        return request()->validate([
+        return $request->validate([
             'name' => 'required|min:5|max:255',
             'description' => 'required',
         ]);
--- app/Http/Controllers/ProjectPublishController.php
+++ app/Http/Controllers/ProjectPublishController.php
@@ -14,6 +14,7 @@ class ProjectPublishController extends Controller
             'published' => $request->published === '0' ? null : now()
         ]);
         $project->save();
+
         return redirect(route('home'));
     }
 }
