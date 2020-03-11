8e55d1a9ec602c78ecce2758a44c23db67779b69
Rene <rene.weiser@uni-weimar.de>
2020-03-11 14:11:02 +0100

    Removing docstrings

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,28 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Project;
 use Illuminate\Http\Request;
-use Spatie\Permission\Models\Permission;
 
 class ProjectController extends Controller
 {
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         $this->authorize('create.projects');
         return view('project.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store()
     {
         $this->authorize('create.projects');
         $project = Project::create($this->validateProject());
@@ -33,37 +21,18 @@ class ProjectController extends Controller
         return view('project.edit', ['project' => $project]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Project  $project
-     * @return \Illuminate\Http\Response
-     */
     public function show(Project $project)
     {
         $this->authorize("show.projects.{$project->id}");
         return view('project.show', ['project' => $project]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Project  $project
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Project $project)
     {
         $this->authorize("edit.projects.{$project->id}");
         return view('project.edit', ['project' => $project]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Project  $project
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Project $project)
     {
         $this->authorize("edit.projects.{$project->id}");
@@ -72,12 +41,6 @@ class ProjectController extends Controller
         return view('project.edit', ['project' => $project]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Project  $project
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Project $project)
     {
         $this->authorize("delete.projects.{$project->id}");
