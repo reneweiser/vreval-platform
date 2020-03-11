6a23a137c6860243e8bbb942617154bca84933bb
René Weiser <rene.weiser@uni-weimar.de>
2020-03-11 19:54:06 +0100

    Paginating projects in home view to 5 per page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home', ['projects' => auth()->user()->projects]);
+        return view('home', ['projects' => auth()->user()->projects()->paginate(5)]);
     }
 }
